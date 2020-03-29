d66677a7ee85f18b0f81aa5a7f5039b5e531ab67
melanie-t <melanie.taing@mail.concordia.ca>
2020-03-29 17:32:04 -0400

    Print statements used for debugging

--- src/NaivesBayesClassification.py
+++ src/NaivesBayesClassification.py
@@ -2,6 +2,7 @@ import re
 
 
 def build_model(vocab, n, lang, tweet, vocabulary, all_lang, ngram_count):
+    # print(lang, tweet)
     for i in range(n - 1, len(tweet)):
         start_index = i - (n - 1)
         ngram = tweet[start_index:i + 1]
@@ -9,12 +10,14 @@ def build_model(vocab, n, lang, tweet, vocabulary, all_lang, ngram_count):
             # Check if ngram hasn't been added to vocabulary yet
             if ngram not in vocabulary:
                 # Add ngram to vocabulary and initialize the value in all other languages
+                # print("New ngram", ngram)
                 vocabulary.append(ngram)
                 for key in all_lang.keys():
                     all_lang[key][ngram] = 0
             # Update ngram count in given lang
             ngram_count[lang] = ngram_count.get(lang) + 1
             all_lang[lang][ngram] = all_lang[lang].get(ngram) + 1
+            # print("\t Updated ", ngram, all_lang[lang][ngram])
 
 
 def valid_ngram(vocab, ngram):
