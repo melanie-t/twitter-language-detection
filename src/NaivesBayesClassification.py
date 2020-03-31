5f0baf88ab10652d5ae034fa97923e9eec804a4e
melanie-t <melanie.taing@mail.concordia.ca>
2020-03-30 23:05:02 -0400

    Implemented vocab2 model to be created as we parse the training data

--- src/NaivesBayesClassification.py
+++ src/NaivesBayesClassification.py
@@ -1,24 +1,31 @@
 import re
+import unicodedata
 
 
-def build_model(vocab, n, lang, tweet, vocabulary, all_lang, ngram_count):
+def build_model(v, n, lang, tweet, all_lang, ngram_count):
     # print(lang, tweet)
     for i in range(n - 1, len(tweet)):
         start_index = i - (n - 1)
         ngram = tweet[start_index:i + 1]
-        if vocab == 0:
+        if v == 0:
             tweet = tweet.lower()
-        if valid_ngram(vocab, ngram):
-            # Check if ngram hasn't been added to vocabulary yet
-            if ngram not in vocabulary:
-                # Add ngram to vocabulary and initialize the value in all other languages
-                # print("New ngram", ngram)
-                vocabulary.append(ngram)
-                for key in all_lang.keys():
-                    all_lang[key][ngram] = 0
+
+        if valid_ngram(v, ngram):
             # Update ngram count in given lang
-            ngram_count[lang] = ngram_count.get(lang) + 1
-            all_lang[lang][ngram] = all_lang[lang].get(ngram) + 1
+            # print(ngram)
+
+            # For v=2, we only add ngrams to vocab as we see them in training set
+            if v == 2:
+                count = all_lang[lang].get(ngram)
+                if count is None:
+                    all_lang[lang][ngram] = 1       # Initialize new ngram
+                else:
+                    all_lang[lang][ngram] = count + 1     # Update existing ngram count
+
+            # For v=0, v=1, we have initialized each language dictionary
+            else:
+                ngram_count[lang] = ngram_count.get(lang) + 1
+                all_lang[lang][ngram] = all_lang[lang].get(ngram) + 1
             # print("\t Updated ", ngram, all_lang[lang][ngram])
 
 
@@ -34,33 +41,38 @@ def valid_ngram(vocab, ngram):
         return False
 
 
-def train_model(vocab, n, smoothing):
+def train_model(v, n, smoothing):
     all_languages = dict()
     ngram_count = dict()
     language_count = dict()
-    vocabulary = []
+    vocabulary = dict()
+
+    # Build vocabulary
+    if v == 0:
+        vocabulary = build_vocab0(n)
+    elif v == 1:
+        vocabulary = build_vocab1_2(n, True)
+    # When v=2, we don't initialize the model
 
     # Initialize dictionaries
     languages = ['eu', 'ca', 'gl', 'es', 'en', 'pt']
     for language in languages:
-        all_languages[language] = dict()
+        all_languages[language] = vocabulary.copy()
         ngram_count[language] = 0
         language_count[language] = 0
 
-    tweet = "abc!DeFg*"
-    tweet2 = "a$bcdefgab*cd*e"
     f = open("OriginalDataSet/training-1.txt", "r", encoding="utf-8")
     training_set = f.readlines()
     for line in training_set:
-        split = line.replace('\n', '').split("\t")
+        split = line.split("\t")
         lang = split[2]
         tweet = split[3]
 
         language_count[lang] = language_count.get(lang) + 1
-        build_model(vocab, n, lang, tweet, vocabulary, all_languages, ngram_count)
+        build_model(v, n, lang, tweet, all_languages, ngram_count)
 
     for lang in all_languages.keys():
-        print(lang, language_count[lang], ngram_count[lang], all_languages[lang])
+        print(lang, language_count[lang], ngram_count[lang])
 
 
 def build_vocab0(n):
@@ -81,7 +93,7 @@ def build_vocab0(n):
     return vocabulary
 
 
-def build_vocab1_2(n, isalpha):
+def build_vocab1_2(n):
     vocabulary = dict()
     A = 65
     a = 97
@@ -96,9 +108,8 @@ def build_vocab1_2(n, isalpha):
             for j in range(0, 26):
                 vocabulary[chr(A+i)+chr(A+j)] = 0        # 1: All upper-case
                 vocabulary[chr(a+i)+chr(a+j)] = 0        # 2: All lower-case
-                if isalpha:
-                    vocabulary[chr(A+i)+chr(a+j)] = 0        # 3: First char upper, second char lower
-                    vocabulary[chr(a+i)+chr(A+j)] = 0        # 4: First char lower, second char upper
+                vocabulary[chr(A+i)+chr(a+j)] = 0        # 3: First char upper, second char lower
+                vocabulary[chr(a+i)+chr(A+j)] = 0        # 4: First char lower, second char upper
 
     elif n == 3:
         for i in range(0, 26):
@@ -106,21 +117,13 @@ def build_vocab1_2(n, isalpha):
                 for k in range(0, 26):
                     vocabulary[chr(A+i)+chr(A+j)+chr(A+k)] = 0       # 1: All upper case (UUU)
                     vocabulary[chr(a+i)+chr(a+j)+chr(a+k)] = 0       # 5: All lower case (LLL)
-                    if isalpha:
-                        vocabulary[chr(a+i)+chr(a+j)+chr(A+k)] = 0       # 6: Lower, Lower, Upper (LLU)
-                        vocabulary[chr(A+i)+chr(A+j)+chr(a+k)] = 0       # 2: Upper, Upper, Lower (UUL)
-                        vocabulary[chr(A+i)+chr(a+j)+chr(A+k)] = 0       # 3: Upper, Lower, Upper (ULU)
-                        vocabulary[chr(a+i)+chr(A+j)+chr(A+k)] = 0       # 4: Lower, Upper, Upper (LUU)
-                        vocabulary[chr(a+i)+chr(A+j)+chr(a+k)] = 0       # 7: Lower, Upper, Lower (LUL)
-                        vocabulary[chr(A+i)+chr(a+j)+chr(a+k)] = 0       # 8: Upper, Lower, Lower (ULL)
+                    vocabulary[chr(a+i)+chr(a+j)+chr(A+k)] = 0       # 6: Lower, Lower, Upper (LLU)
+                    vocabulary[chr(A+i)+chr(A+j)+chr(a+k)] = 0       # 2: Upper, Upper, Lower (UUL)
+                    vocabulary[chr(A+i)+chr(a+j)+chr(A+k)] = 0       # 3: Upper, Lower, Upper (ULU)
+                    vocabulary[chr(a+i)+chr(A+j)+chr(A+k)] = 0       # 4: Lower, Upper, Upper (LUU)
+                    vocabulary[chr(a+i)+chr(A+j)+chr(a+k)] = 0       # 7: Lower, Upper, Lower (LUL)
+                    vocabulary[chr(A+i)+chr(a+j)+chr(a+k)] = 0       # 8: Upper, Lower, Lower (ULL)
 
     return vocabulary
 
-
-# train_model(0, 1, 1)
-# vocab0(3)
-vocab = build_vocab1_2(1, False)
-print(vocab)
-print(len(vocab))
-
-
+train_model(2, 1, 0)
